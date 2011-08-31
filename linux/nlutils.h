66898d2a211d516d848c4ca23efe0673251f4659
Thomas Pedersen <thomas@cozybit.com>
2011-08-30 17:15:02 -0700

    read HT info from wiphy
    We start maintaining wiphy info in nlcfg. This is wrong, and should be
    factored out later into some sort of meshd hwinfo/state struct. On the
    bright side, scaffolding is now in place to grab the rates. We shouldn't
    store those in nlcfg either...
    
    Also, give meshd-nl80211 a vim modeline, and rename request_mesh_caps to
    get_wiphy.

--- linux/nlutils.h
+++ linux/nlutils.h
@@ -30,18 +30,41 @@
 #include <linux/if_ether.h>
 #include "nl80211-copy.h"
 
+/* leave hw/kernel info here for now, maybe move to common.h? */
+#include "stdbool.h"
+#define IEEE80211_NUM_BANDS	2
+
+struct mcs_info {
+        uint8_t rx_mask[10];
+        uint16_t rx_highest;
+        uint8_t tx_params;
+        uint8_t reserved[3];
+} __attribute__((packed));
+
+struct local_ht_caps {
+	uint16_t cap;
+	bool ht_supported;
+	uint8_t ampdu_factor;
+	uint8_t ampdu_density;
+	struct mcs_info mcs;
+};
+
+struct ieee80211_band {
+	struct local_ht_caps ht_cap;
+};
 
 struct netlink_config_s {
 	struct nl_sock *nl_sock;
 	struct nl_cache *nl_cache;
 	struct nl_sock *nl_sock_event;
 	struct nl_cache *nl_cache_event;
-    struct nl_cb *nl_cb;
+	struct nl_cb *nl_cb;
 	struct genl_family *nl80211;
-    int ifindex;
-    int freq;
-    uint8_t mymacaddr[ETH_ALEN];
-    int supress_error;
+	int supress_error;
+	int ifindex;
+	int freq;
+	uint8_t mymacaddr[ETH_ALEN];
+	struct ieee80211_band bands[IEEE80211_NUM_BANDS];
 };
 
 int netlink_init(struct netlink_config_s *nlcfg, void *event_handler);

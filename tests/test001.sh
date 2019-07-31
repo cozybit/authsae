7677a6359acebe1ed95a9c399729d6f1935740f9
Bob Copeland <me@bobcopeland.com>
2019-07-31 16:01:03 -0400

    authsae: add timestamps to log output
    This adds some timestamps to make it easier to debug state machine
    interactions with multiple stations.  Example output:
    
    [15:49:45.099684] Mesh plink: starting establishment with 02:00:00:00:00:00
    [15:49:45.099687] Mesh plink: Sending plink action 1
    
    Fix up some tests that break due to not expecting the timestamps.

--- tests/test001.sh
+++ tests/test001.sh
@@ -23,19 +23,19 @@ LOG0=${LOGS[0]}
 LOG1=${LOGS[1]}
 
 # pmk match
-cat ${LOG0} | grep pmk -A 2 > ${TMP0}
-cat ${LOG1} | grep pmk -A 2 > ${TMP1}
+sed -e 's/.*\] //' ${LOG0} | grep pmk -A 2 > ${TMP0}
+sed -e 's/.*\] //' ${LOG1} | grep pmk -A 2 > ${TMP1}
 
 diff ${TMP0} ${TMP1} || { echo "pmk mismatch"; cat ${TMP0} ${TMP1}; err_exit "pmk mismatch"; }
 
 # mgtk exchange in both directions
-cat ${LOG0} | grep ^mgtk -A 1 | tail -1 > ${TMP0}
-cat ${LOG1} | grep "Received mgtk:" -A 1 | tail -1 > ${TMP1}
+sed -e 's/.*] //' ${LOG0} | grep ^mgtk -A 1 | tail -1 > ${TMP0}
+sed -e 's/.*] //' ${LOG1} | grep "Received mgtk:" -A 1 | tail -1 > ${TMP1}
 
 diff ${TMP0} ${TMP1} || { echo "mgtk mismatch"; cat ${TMP0} ${TMP1}; err_exit "mgtk exchange failed"; }
 
-cat ${LOG0} | grep "Received mgtk:" -A 1 | tail -1 > ${TMP0}
-cat ${LOG1} | grep ^mgtk -A 1 | tail -1 > ${TMP1}
+sed -e 's/.*] //' ${LOG0} | grep "Received mgtk:" -A 1 | tail -1 > ${TMP0}
+sed -e 's/.*] //' ${LOG1} | grep ^mgtk -A 1 | tail -1 > ${TMP1}
 
 diff ${TMP0} ${TMP1} || { echo "mgtk mismatch"; cat ${TMP0} ${TMP1}; err_exit "mgtk exchange failed"; }
 
